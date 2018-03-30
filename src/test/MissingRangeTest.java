20b38aaffb7b8302118f087922d912530bb555b1
Weijie Lin <weijie@gmail.com>
2018-03-29 21:19:18 -0700

    added unique path recursive dp solution

--- src/test/MissingRangeTest.java
+++ src/test/MissingRangeTest.java
@@ -2,18 +2,18 @@ package test;
 
 import main.MissingRange;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class MissingRangeTest {
+public class MissingRangeTest {
 
     @Test
-    void testMissingRange() {
+    public void testMissingRange() {
        MissingRange range = new MissingRange();
        Assert.assertArrayEquals(new String[]{"2","4->49","51->74","76->99"}, range.getMissingRange(new Integer[]{0, 1, 3, 50, 75}, 0, 99).toArray());
     }
 
     @Test
-    void testEmptyString() {
+    public void testEmptyString() {
         MissingRange range = new MissingRange();
         Assert.assertArrayEquals(new String[]{"0->99"}, range.getMissingRange(new Integer[]{}, 0, 99).toArray());
     }
